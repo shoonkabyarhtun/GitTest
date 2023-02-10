ffcaad449644a25ac4331e8b93fa0e3b8b716c45
shoonkabyarhtun <110212653+shoonkabyarhtun@users.noreply.github.com>
2023-02-10 23:20:48 +0630

    Update HelloWorld.java

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -10,7 +10,6 @@ public class HelloWorld {
 
 	System.out.println("Inside Developer_001 Branch");
 	System.out.println("to test shopping cart features");
-	
-	}
+	System.out.println("I changed from remote master");
 
 }
